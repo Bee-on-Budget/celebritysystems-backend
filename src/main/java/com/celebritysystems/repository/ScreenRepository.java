cdda3f83dd44680fa9885cb025f5170b7b41ac20
Salwan Arar <salwan.arar@gmail.com>
2025-05-07 18:27:03 +0300

    Added Contract and Screen repositories

---
+++ src/main/java/com/celebritysystems/repository/ScreenRepository.java
@@ -0,0 +1,9 @@
+package com.celebritysystems.repository;
+
+import com.celebritysystems.entity.Screen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
+} 
