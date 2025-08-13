3ce7cd714ece0f423aa9d1ae7de31ef15f45bd41
MohammedSkouti <mouh1998s@gmail.com>
2025-08-13 20:06:56 +0300

    add user pagination

--- src/main/java/com/celebritysystems/service/UserService.java
+++ src/main/java/com/celebritysystems/service/UserService.java
@@ -2,7 +2,8 @@ package com.celebritysystems.service;
 
 import com.celebritysystems.entity.User;
 import com.celebritysystems.entity.enums.RoleInSystem;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -19,6 +20,7 @@ public interface UserService {
     User updateUser(Long id, User user);
     User patchUser(Long id, Map<String, Object> updates);
     boolean resetPassword(Long userId, String newPassword);
+    Page<User> findAllPaginated(Pageable pageable, String search, RoleInSystem role, Long companyId);
 
 //    List<UserRegistrationStatsDTO> getUserRegistrationStats();
 }
